d3d176193e81f484c6a1e37535f2fd690203de09
xonx <119700621+xonx4l@users.noreply.github.com>
2024-11-21 23:16:13 +0530

    ADD Fns

--- Atomics.rs
+++ Atomics.rs
@@ -12,6 +12,38 @@ unsafe impl Sync for AtomicUsize {}
 impl AtomicUsize {
 
 }
+
+pub const fn new(v: usize) -> Self {
+    Self {
+        inner: UnsafeCell::new(v),
+    }
+}
+
+pub fn load(&self) -> usize {
+    unsafe { *self.inner.get() }
+}
+
+pub fn store(&self, v: usize) {
+    unsafe {
+        asm!(
+            "lock; xchg [{address}], {v}",
+            address = in(reg) self.inner.get(),
+            v = in(reg) v
+        );
+    }
+}
+
+pub fn fetch_add(&self, mut v: usize) -> usize {
+    unsafe {
+        asm!(
+            "lock; xadd [{address}], {v}",
+            address = in(reg) self.inner.get(),
+            v = inout(reg) v,
+        );
+    }
+
+    v
+}
   
 
 
